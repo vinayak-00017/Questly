f755bd98264cc6e6410309df391d5654176acad0
vinayak <fobo401@gmail.com>
2025-07-20 07:38:21 +0530

    server port fix, tsup config fix

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -3,11 +3,6 @@ import { defineConfig, type Options } from "tsup";
 export default defineConfig((options: Options) => ({
   entryPoints: ["src/index.ts"],
   clean: true,
-  format: ["cjs"], // Use CommonJS format for better Docker compatibility
-  outExtension: () => {
-    return {
-      js: ".js", // Keep .js extension but output CommonJS
-    };
-  },
+  format: ["cjs"],
   ...options,
 }));
